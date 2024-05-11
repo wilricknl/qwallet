37b9224ec4d0e1ec2d62992e63090689cd5dd5d1
wilricknl <83393813+wilricknl@users.noreply.github.com>
2024-05-11 14:35:11 +0200

    feat: query tick information

---
+++ include/network/tick.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <tl/expected.hpp>
+
+#include "network/connection.hpp"
+#include "network_messages/tick.h"
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * Get current tick info from a node
+ * @param connection The node to query
+ * @return The tick info upon success, else a connection error
+ */
+tl::expected<CurrentTickInfo, ConnectionError> getCurrentTickInfo(const ConnectionPtr& connection);
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * Get the current tick duration
+ * @param connection The node to query
+ * @return The current tick duration upon success, else a connection error
+ */
+tl::expected<unsigned short, ConnectionError> getTickDuration(const ConnectionPtr& connection);
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * Get the current epoch
+ * @param connection The node to query
+ * @return The current epoch upon success, else a connection error
+ */
+tl::expected<unsigned short, ConnectionError> getEpoch(const ConnectionPtr& connection);
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * Get the current tick number
+ * @param connection The node to query
+ * @return The current tick number upon success, else a connection error
+ */
+tl::expected<unsigned int, ConnectionError> getTick(const ConnectionPtr& connection);
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * Get the number of aligned votes of the current tick
+ * @param connection The node to query
+ * @return The number of aligned votes upon success, else a connection error
+ */
+tl::expected<unsigned short, ConnectionError> getNumberOfAlignedVotes(
+    const ConnectionPtr& connection);
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * Get the number of misaligned votes of the current tick
+ * @param connection The node to query
+ * @return The number of misaligned votes upon success, else a connection error
+ */
+tl::expected<unsigned short, ConnectionError> getNumberOfMisalignedVotes(
+    const ConnectionPtr& connection);
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * Get the initial tick of the epoch
+ * @param connection The node to query
+ * @return The initial tick of the epoch upon success, else a connection error
+ */
+tl::expected<unsigned int, ConnectionError> getInitialTick(const ConnectionPtr& connection);
---
+++ src/network/tick.cpp
@@ -0,0 +1,90 @@
+#include "network/tick.hpp"
+
+#include <cstring>
+
+// ------------------------------------------------------------------------------------------------
+tl::expected<CurrentTickInfo, ConnectionError> getCurrentTickInfo(const ConnectionPtr& connection)
+{
+    // construct request packet
+    struct
+    {
+        RequestResponseHeader header;
+    } packet;
+    packet.header.setSize<sizeof(packet)>();
+    packet.header.randomizeDejavu();
+    packet.header.setType(REQUEST_CURRENT_TICK_INFO);
+
+    // send request
+    connection->Send((char*)&packet, sizeof(packet));
+
+    // receive response
+    return connection->ReceiveAs<CurrentTickInfo>(RESPOND_CURRENT_TICK_INFO);
+}
+
+// ------------------------------------------------------------------------------------------------
+tl::expected<unsigned short, ConnectionError> getTickDuration(const ConnectionPtr& connection)
+{
+    auto info = getCurrentTickInfo(connection);
+    if (info)
+    {
+        return info->tickDuration;
+    }
+    return tl::make_unexpected(info.error());
+}
+
+// ------------------------------------------------------------------------------------------------
+tl::expected<unsigned short, ConnectionError> getEpoch(const ConnectionPtr& connection)
+{
+    auto info = getCurrentTickInfo(connection);
+    if (info)
+    {
+        return info->epoch;
+    }
+    return tl::make_unexpected(info.error());
+}
+
+// ------------------------------------------------------------------------------------------------
+tl::expected<unsigned, ConnectionError> getTick(const ConnectionPtr& connection)
+{
+    auto info = getCurrentTickInfo(connection);
+    if (info)
+    {
+        return info->tick;
+    }
+    return tl::make_unexpected(info.error());
+}
+
+// ------------------------------------------------------------------------------------------------
+tl::expected<unsigned short, ConnectionError> getNumberOfAlignedVotes(
+    const ConnectionPtr& connection)
+{
+    auto info = getCurrentTickInfo(connection);
+    if (info)
+    {
+        return info->numberOfAlignedVotes;
+    }
+    return tl::make_unexpected(info.error());
+}
+
+// ------------------------------------------------------------------------------------------------
+tl::expected<unsigned short, ConnectionError> getNumberOfMisalignedVotes(
+    const ConnectionPtr& connection)
+{
+    auto info = getCurrentTickInfo(connection);
+    if (info)
+    {
+        return info->numberOfMisalignedVotes;
+    }
+    return tl::make_unexpected(info.error());
+}
+
+// ------------------------------------------------------------------------------------------------
+tl::expected<unsigned, ConnectionError> getInitialTick(const ConnectionPtr& connection)
+{
+    auto info = getCurrentTickInfo(connection);
+    if (info)
+    {
+        return info->initialTick;
+    }
+    return tl::make_unexpected(info.error());
+}
